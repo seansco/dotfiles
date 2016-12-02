560f20c9ceaac0aa5de30ca36b750a98960d8cb4
Chris Lane <chris@chris-allen-lane.com>
2016-12-01 22:11:07 -0500

    awm: changed some tags

--- awesome/config/tags.lua
+++ awesome/config/tags.lua
@@ -6,14 +6,14 @@ local exports = {}
 
 -- top
 exports[1] = awful.tag(
-  { '1.com  ', '2.med  ', '3.btc  ', '4.etc  ', '5.vol' },
+  { '1.irc  ', '2.med  ', '3.btc  ', '4.bnc  ', '5.etc  ', '6.vol' },
   1,
   layouts[1]
 )
 
 -- right
 exports[2] = awful.tag(
-  { '1.www  ', '2.www  ', '3.www  ', '4.www  ', '5.soc' },
+  { '1.www  ', '2.www  ', '3.www  ', '4.www  ', '5.www  ', '6.soc' },
   2,
   layouts[3]
 )
@@ -27,7 +27,7 @@ exports[3] = awful.tag(
 
 -- left
 exports[4] = awful.tag(
-  { '1.trm  ', '2.trm  ', '3.trm  ', '4.www  ', '5.etc' },
+  { '1.trm  ', '2.trm  ', '3.trm  ', '4.trm  ', '5.trm ', '6.mai' },
   4,
   layouts[3]
 )
